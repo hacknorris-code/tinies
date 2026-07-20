3be9d9fcbae612d19d33793f1f839ea5fe933782
hacknorris <hacknorris-dev@proton.me>
2026-07-20 02:38:36 +0200

    image fix

--- gzip_check.sh
+++ gzip_check.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-for f in *.html; do
-  [ -e "$f" ] || continue
-  gz=$(gzip -c9 "$f" | wc -c)
-  echo "$gz  $f"
+find . -maxdepth 1 -type f | while IFS= read -r f; do
+    orig=$(wc -c < "$f")
+    gz=$(gzip -c9 "$f" | wc -c)
+    br=$(brotli -q 11 -c "$f" | wc -c)
+    printf "%10d  %10d  %10d  %s\n" \
+        "$orig" "$gz" "$br" "$f"
 done
